5126f9cf42ad45837ce3c86093fc5833cf635195
wenfs <48020947+wenfs@users.noreply.github.com>
2020-03-03 19:44:54 +0800

    Update we.py

--- Test01/we.py
+++ Test01/we.py
@@ -7,7 +7,7 @@ c = int(input("请输入任意c值："))
 print("您输入的a,b,c为：%d,%d,%d"%(a,b,c))
 da=b**2-4*a*c
 if da < 0 :
-    print("该方程在您设置的变量下无解")
+    print("该方程在您设置的参数下无解")
 elif da > 0:
     print("该方程有两个解！")
     x1=(-b+cmath.sqrt(da))/(2*a)
