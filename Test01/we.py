c0bce5d85cc57069e640809d364b72e1f7addb96
wenfs <48020947+wenfs@users.noreply.github.com>
2020-03-03 18:39:57 +0800

    Add files via upload

---
+++ Test01/we.py
@@ -0,0 +1,20 @@
+import tensorflow
+import cmath
+
+a = int(input("请输入任意a值："))
+b = int(input("请输入任意b值："))
+c = int(input("请输入任意c值："))
+print("您输入的a,b,c为：%d,%d,%d"%(a,b,c))
+da=b**2-4*a*c
+if da < 0 :
+    print("该方程在您设置的变量下无解")
+elif da > 0:
+    print("该方程有两个解！")
+    x1=(-b+cmath.sqrt(da))/(2*a)
+    x2=(-b-cmath.sqrt(da))/(2*a)
+    print("x1和x2分别为：{} {}".format(x1,x2))
+else:
+    print("该方程有且仅有一个解")
+    print("x的值为：%d"%(-b/(2*a)))
+
+
