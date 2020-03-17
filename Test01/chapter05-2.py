d1e822ffb723e5883ef437c839ec8e884c5862e2
wenfs <48020947+wenfs@users.noreply.github.com>
2020-03-17 17:52:24 +0800

    Update chapter05-2.py

--- Test01/chapter05-2.py
+++ Test01/chapter05-2.py
@@ -1,15 +1,15 @@
 import numpy as np
+x=np.array([64.3,99.6,145.45,63.75,135.46,92.85,86.97,144.76,59.3,116.03])
+y=np.array([62.55,82.42,132.62,73.31,131.05,86.57,85.49,127.44,55.25,104.84])
+#x\y的均值
+aveX = np.mean(x)
+aveY = np.mean(y)
 
-x=[64.3,99.6,145.45,63.75,135.46,92.85,86.97,144.76,59.3,116.03]
-y=[62.55,82.42,132.62,73.31,131.05,86.57,85.49,127.44,55.25,104.84]
-aveX = sum(x)/10
-aveY = sum(y)/10
-sum1=sum2=0
-for i in range(10):
-    sum1+=(x[i]-aveX)*(y[i]-aveY)
-for j in range(10):
-    sum2+=(x[i]-aveX)**2
+sum1 = np.sum((x-aveX)*(y-aveY))
+sum2 = np.sum(np.power((x-aveX),2))
+
 w=sum1/sum2
 b=aveY-w*aveX
-print("W的值为：%d"%w)
-print("b的值为：%d"%b)
+
+print("W的值为：%f"%w)
+print("b的值为：%f"%b)
